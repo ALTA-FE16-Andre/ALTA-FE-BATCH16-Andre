b612c828792e12a7f8c59854d998987c3f22cf89
andreputra8 <contact.andreputra@gmail.com>
2023-10-19 18:48:09 +0700

    feat: finish task part 10

--- 10-DnC-and-Dynamic-Programming/problem3.ts
+++ 10-DnC-and-Dynamic-Programming/problem3.ts
@@ -1,15 +1,15 @@
 function frog(jumps: number[]): number {
   // your code here
-  let toJump: number[] = [0, Math.abs(jumps[0] - jumps[1])];
+  let result: number[] = [0, Math.abs(jumps[0] - jumps[1])];
 
   for (let i = 2; i < jumps.length; i++) {
-    toJump[i] = Math.min(
-      toJump[i - 1] + Math.abs(jumps[i - 1] - jumps[i]),
-      toJump[i - 2] + Math.abs(jumps[i - 2] - jumps[i])
+    result[i] = Math.min(
+      result[i - 1] + Math.abs(jumps[i - 1] - jumps[i]),
+      result[i - 2] + Math.abs(jumps[i - 2] - jumps[i])
     );
   }
 
-  return toJump[jumps.length - 1];
+  return result[jumps.length - 1];
 }
 
 console.log(frog([10, 30, 40, 20])); // 30
